fae0681012583f31cf5e041e464b38db7eb3fcd7
hari29harran <63388630+hari29harran@users.noreply.github.com>
2020-08-12 11:19:38 +0530

    Add files via upload

---
+++ precache-manifest.71a40dbb3e3a22cbca9a4bf39835a8c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cff5919ba8b5215d9578e56062b6aa",
+    "url": "/hari29harran/covid19-tamilnadu/index.html"
+  },
+  {
+    "revision": "a45ac2e76ac4862c973c",
+    "url": "/hari29harran/covid19-tamilnadu/static/css/2.1f78b77a.chunk.css"
+  },
+  {
+    "revision": "5fdb79b7dff864742ff0",
+    "url": "/hari29harran/covid19-tamilnadu/static/css/main.d2def7dd.chunk.css"
+  },
+  {
+    "revision": "a45ac2e76ac4862c973c",
+    "url": "/hari29harran/covid19-tamilnadu/static/js/2.9abcf613.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/hari29harran/covid19-tamilnadu/static/js/2.9abcf613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdb79b7dff864742ff0",
+    "url": "/hari29harran/covid19-tamilnadu/static/js/main.a43e97a4.chunk.js"
+  },
+  {
+    "revision": "726fbb8a6fbce5d02954",
+    "url": "/hari29harran/covid19-tamilnadu/static/js/runtime-main.3c2302bb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hari29harran/covid19-tamilnadu/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hari29harran/covid19-tamilnadu/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hari29harran/covid19-tamilnadu/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hari29harran/covid19-tamilnadu/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hari29harran/covid19-tamilnadu/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
